7fd10cc54a84afe03179c347b141f04a3502504c
ZWQ <zhong0102@qq.com>
2018-06-01 23:20:16 +0800

    引入vue-router

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -26,7 +26,7 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
     #topbar{
         padding: 16px;
         border: 1px solid blue;
---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login" id="login">
+
+  </div>
+</template>
+
+
+<style lang="scss">
+    .login{
+        width: 100;
+        height: 100%;
+        background: red;
+    }
+</style>
+yy
